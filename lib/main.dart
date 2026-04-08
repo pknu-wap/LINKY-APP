c6b55a2ccfcf1e6be045887c35421cd0a032ed72
4mas395 <112306555+4mas395@users.noreply.github.com>
2026-04-08 09:24:38 +0900

    0408Category

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const linky());
+  runApp(const Linky());
 }
 
-class linky extends StatelessWidget {
-  const linky({super.key});
+class Linky extends StatelessWidget {
+  const Linky({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(title: 'Linky', home: const CategoryPage());
   }
 }
 
@@ -18,6 +18,36 @@ class CategoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 3, // 탭 개수 지정
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Image.asset('assets/images/category_icon.png', width: 56),
+          title: const Text('Category'),
+        ),
+        body: Column(
+          children: const [
+            TabBar(
+              labelColor: Colors.blue,
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Tab(text: '갯수 / 카테고리 제목'),
+                Tab(text: '갯수 / 카테고리 제목'),
+                Tab(text: '갯수 / 카테고리 제목'),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(child: Text("Category 1 Page")),
+                  Center(child: Text("Category 2 Page")),
+                  Center(child: Text("Category 3 Page")),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
