95061b490d58aef85a170b5b88975e6afb216cdb
pepertuna <pepertuna@gmail.com>
2026-04-17 13:57:04 +0900

    separated PopupMenuButton

--- lib/widgets/contents_box.dart
+++ lib/widgets/contents_box.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:std/widgets/alert.dart';
-import 'package:std/widgets/edit_content_sheet.dart';
+import 'package:std/widgets/popup_menu_button.dart';
 
 const mainGreen = Color(0xff3fd966);
 
@@ -61,7 +60,10 @@ class ContentsBox extends StatelessWidget {
                           fontSize: 16,
                         ),
                       ),
-                      _popupMenu(context),
+                      PopupButton(
+                        onActionDone: onActionDone,
+                        context: context,
+                      ),
                     ],
                   ),
                 ),
@@ -100,91 +102,6 @@ class ContentsBox extends StatelessWidget {
       onTap: () {
         print('clicked $titleText');
       },
-    );
-  }
-
-  Widget _popupMenu(BuildContext context) {
-    return PopupMenuButton<String>(
-      offset: const Offset(-10, 40),
-      menuPadding: EdgeInsets.symmetric(vertical: 3),
-      // 메뉴 전체의 최대 너비 제한
-      constraints: const BoxConstraints(
-        maxWidth: 100,
-      ),
-
-      padding: EdgeInsets.zero,
-      icon: const Icon(Icons.more_vert),
-
-      onSelected: (value) {
-        if (value == 'edit') {
-          showDialog(
-            context: context,
-            barrierDismissible: true,
-            builder: (context) {
-              return DialogPopup(
-                title: '해당 링크를 수정하시겠어요?',
-                onConfirm: () {
-                  showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true, // 키보드 대응 및 높이 조절을 위해 필수
-                    backgroundColor:
-                        Colors.transparent, // 배경을 투명하게 해야 컨테이너 디자인이 보임
-                    builder: (context) => const EditContentSheet(),
-                  );
-                },
-                confirmText: '수정',
-              );
-            },
-          );
-        } else if (value == 'delete') {
-          showDialog(
-            context: context,
-            barrierDismissible: true,
-            builder: (context) {
-              return DialogPopup(
-                title: '해당 링크를 삭제하시겠어요?',
-                onConfirm: onActionDone,
-                confirmText: '삭제',
-              );
-            },
-          );
-        }
-      },
-
-      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-        PopupMenuItem<String>(
-          value: 'edit',
-          height: 25,
-
-          padding: const EdgeInsets.symmetric(
-            horizontal: 12,
-          ),
-          child: Row(
-            children: const [
-              Icon(Icons.chevron_right, size: 16),
-              SizedBox(width: 8),
-              Text('수정', style: TextStyle(fontSize: 13)),
-            ],
-          ),
-        ),
-        const PopupMenuDivider(height: 1), // 구분선 높이
-        PopupMenuItem<String>(
-          value: 'delete',
-          height: 25,
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Row(
-            children: const [
-              Icon(Icons.chevron_right, size: 16),
-              SizedBox(width: 8),
-              Text('삭제', style: TextStyle(fontSize: 13)),
-            ],
-          ),
-        ),
-      ],
-      color: Colors.grey[200],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
     );
   }
 }
