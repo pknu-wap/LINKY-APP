05a44fe0867e5fc3c450d8737f349b158b44f3dc
hide-squid <developer.tbk@gmail.com>
2026-04-29 02:45:52 +0900

    setting page complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:std/pages/category_page.dart';
 import 'package:std/pages/private_page.dart';
+import 'package:std/pages/setting.dart';
 import 'package:std/pages/slidepage.dart';
 import 'package:std/std/pages/add_link_page.dart';
 //import 'package:std/std/pages/login.dart';
@@ -42,7 +43,7 @@ class _MainScreenState extends State<MainScreen> {
     const SecretGuardWrapperPw(child: PrivatePage()), // 커스텀 패스워드 (현재 0000)
     const AddLinkPage(),
     const Slidepage(),
-    const Center(child: Text('설정 페이지', style: TextStyle(fontSize: 24))),
+    const Setting(),
   ];
 
   // 탭 클릭 시 인덱스 변경 함수
@@ -121,11 +122,7 @@ class _MainScreenState extends State<MainScreen> {
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: _buildCommonItem(
-                  Icons.settings_outlined,
-                  '설정',
-                  false,
-                ),
+                icon: _buildCommonItem(Icons.settings_outlined, '설정', false),
                 activeIcon: _buildCommonItem(
                   Icons.settings_outlined,
                   '설정',
