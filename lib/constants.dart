ab4d8d2279945be6d1fa3544fd2764ad3cb0b084
pepertuna <pepertuna@gmail.com>
2026-04-30 23:13:07 +0900

    refactor:공용 색 파일 추가

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color bottNavTextGrey = Color(0xFF49454F);
+  static const Color hintTextGrey = Color(0xFF7E7E7E);
+  static const Color outlineGrey = Color(0xFFC5C5C5);
+  static const Color mainYellow = Color(0xFFFFE100);
+  static const Color mainGreen = Color(0xff3fd966);
+  static const Color iconGreen = Color(0xFF34C759);
+  static const Color logoGreen = Color(0xFF1BDC42);
+  static const Color newLinkDoneGreen = Color(0xFFD7FFE1);
+  static const Color mainRed = Color(0xFFFF0000);
+  static const Color mainPink = Color(0xFFFFC0F3);
+  static const Color logoutPink = Color(0xFFFFE9E9);
+  static const Color mainBlue = Color(0xFF0F63FF);
+  static const Color logoBlue = Color(0xFF3978FF);
+  static const Color dateBlue = Color(0xFFE0EFFF);
+}
