1114cf70ddfdd1c1afae64548c2bf1e88f8fda04
pepertuna <pepertuna@gmail.com>
2026-05-01 16:36:28 +0900

    fix:주석 제거

--- lib/widgets/plus_page_category.dart
+++ lib/widgets/plus_page_category.dart
@@ -1,55 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:std/constants.dart';
 
-// class CategoryWidget extends StatelessWidget {
-//   final String? selectedCategory;
-//   final List<String> categories;
-//   final ValueChanged<String?> onChanged;
-
-//   const CategoryWidget({
-//     super.key,
-//     required this.selectedCategory,
-//     required this.categories,
-//     required this.onChanged,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return DropdownButtonFormField<String>(
-//       value: selectedCategory,
-//       decoration: InputDecoration(
-//         hintText: '카테고리',
-//         filled: true,
-//         fillColor: AppColors.white,
-//         contentPadding: const EdgeInsets.symmetric(
-//           horizontal: 16,
-//           vertical: 18,
-//         ),
-//         border: OutlineInputBorder(
-//           borderRadius: BorderRadius.circular(14),
-//           borderSide: const BorderSide(color: AppColors.black),
-//         ),
-//       ),
-
-//       dropdownColor: AppColors.white,
-//       borderRadius: BorderRadius.circular(14),
-//       icon: const Icon(Icons.arrow_drop_down, color: AppColors.black),
-//       items: categories.map((category) {
-//         return DropdownMenuItem<String>(
-//           value: category,
-//           child: Text(
-//             category,
-//             style: const TextStyle(fontSize: 16, color: AppColors.black),
-//           ),
-//         );
-//       }).toList(),
-//       onChanged: onChanged,
-//     );
-//   }
-// }
-
 class CategoryWidget extends StatefulWidget {
-  // 상태 관리를 위해 StatefulWidget으로 변경
   const CategoryWidget({
     super.key,
     required this.onCategorySelected, // 값이 바뀌었을 때 실행할 콜백
@@ -72,8 +24,8 @@ class _CategoryWidgetState extends State<CategoryWidget> {
         borderRadius: BorderRadius.circular(8),
 
         side: const BorderSide(
-          color: AppColors.black, // 테두리 색상
-          width: 0.5, // 테두리 두께
+          color: AppColors.black,
+          width: 0.5,
         ),
       ),
 
@@ -89,7 +41,7 @@ class _CategoryWidgetState extends State<CategoryWidget> {
       ),
 
       onSelected: (value) {
-        widget.onCategorySelected(value); // 부모에게 값 전달
+        widget.onCategorySelected(value);
       },
       itemBuilder: (BuildContext context) {
         List<PopupMenuEntry<String>> menuList = [];
